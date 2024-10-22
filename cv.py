c748502109b957f35419673ad88e52a89dccce16
Christian Ray <143613191+CARay1502@users.noreply.github.com>
2024-10-22 17:16:53 -0400

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,35 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(r'C:\Users\crayc\OneDrive\Desktop\computerVision\haarcascade_frontalface_default.xml')
+face_count = 0
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error: Failed to capture image.")
+        break
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        if (faces.any()):
+            cv2.putText(frame, "Face Detected", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+
+    cv2.imshow('Camera Feed', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
